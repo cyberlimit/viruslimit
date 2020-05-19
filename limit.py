95961c04670fd0890bc19a0511f0bbed6fb5e138
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-05-20 04:48:13 +0700

    Create limit.py

---
+++ limit.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+#jangan recode bangsatt 
+#apa lu jing 
+
+os.system('clear')
+os.system('figlet Cyber')
+print "-----------------------------------------------------"
+print "------Autor = Cyber Limit----------------------------" 
+print "------My Tim = Wisnu | panjull-----------------------"
+print "------Youtube= Cyber Limit --------------------------"
+print "-----------------------------------------------------"
+print 
+print "[+]Menu Virus[+]
+print ""
+print "[1]. Virus Trojans "
+print "[2]. DDOS Attack "
+print "[3]. FB MBF"
+pilih= raw_input('[?] pilih : ')
+
