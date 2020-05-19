901d2a6891cb740374e86f42e9668b695500464c
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-05-20 06:47:15 +0700

    Update limit.py

--- limit.py
+++ limit.py
@@ -5,17 +5,38 @@ import sys
 #apa lu jing 
 
 os.system('clear')
-os.system('figlet Cyber')
-print "-----------------------------------------------------"
-print "------Autor = Cyber Limit----------------------------" 
-print "------My Tim = Wisnu | panjull-----------------------"
-print "------Youtube= Cyber Limit --------------------------"
-print "-----------------------------------------------------"
+time.sleep(1)
+os.system('figlet CyberLimit')
+time.sleep(1)
+print "==========================================="
+print "------Autor = Cyber Limit----" 
+print "------My Tim = Wisnu | panjull----"
+print "------Youtube= Cyber Limit -----"
+print "==========================================="
+time.sleep(1)
 print 
-print "[+]Menu Virus[+]"
+print "[+]Menu nya Cukkkkk[+]"
+time.sleep(1)
 print ""
 print "[1]. Virus Trojans "
+time.sleep(1)
 print "[2]. DDOS Attack "
+time.sleep(1)
 print "[3]. FB MBF"
+time.sleep(1)
 pilih= raw_input('[?] pilih : ')
+
+
+
+os.system('git clone https://github.com/Bl4ckDr460n/STrojan.py')
+          print "penginstalan selesai"
+          print "[+]Kalian ketik $ STrojan"
+          
+      os.system('clear')
+       print "[+]Lalu Ketik $ python2 STrojan.py"
+
+
+
+
+
 
